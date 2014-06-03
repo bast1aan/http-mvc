6d8cb437bc3b55d1e3cb385eee8ba5cb6eef9125
Bastiaan Welmers <bastiaan@welmers.net>
2014-06-03 20:26:11 +0200

    Remove doGet() doPost() doPut() doDelete() doRequest() should suffice

--- src/Bast1aan/HttpMvc/AbstractController.php
+++ src/Bast1aan/HttpMvc/AbstractController.php
@@ -44,32 +44,5 @@ abstract class AbstractController {
 	 */
 	public function doRequest(Request $request, Response $response) {}
 
-	/**
-	 * @param Request $request
-	 * @param Response $response
-	 * @throws NotFoundException|ServerErrorException
-	 */
-	public function doGet(Request $request, Response $response) {}
-
-	/**
-	 * @param Request $request
-	 * @param Response $response
-	 * @throws NotFoundException|ServerErrorException
-	 */
-	public function doPut(Request $request, Response $response) {}
-
-	/**
-	 * @param Request $request
-	 * @param Response $response
-	 * @throws NotFoundException|ServerErrorException
-	 */
-	public function doPost(Request $request, Response $response) {}
-
-	/**
-	 * @param Request $request
-	 * @param Response $response
-	 * @throws NotFoundException|ServerErrorException
-	 */
-	public function doDelete(Request $request, Response $response) {}
 
 }
